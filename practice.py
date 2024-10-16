d32e0575a4c573438b2f5e11b2dba8c17136a88c
chyifeng <chenyifeng@gmail.com>
2024-10-16 13:48:41 +0800

    测试

---
+++ practice.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+x, y, z = axes3d.get_test_data()
+
+ax.plot_wireframe(x, y, z, rstride = 3, cstride = 3)
+
+plt.title("Wireframe Plot Example")
+plt.tight_layout()
+plt.show()
